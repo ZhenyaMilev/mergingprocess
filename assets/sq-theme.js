fb6d87f228c726197b4da1eaf3c58b893b2f047a
MacBook Air <macbookair@Noutbuk-MacBook.local>
2025-01-28 10:40:32 +0200

    first

---
+++ assets/sq-theme.js
@@ -0,0 +1,21 @@
+/* These files are auto-generated, do not modify them. If you wish to modify certain assets, please use the sq-custom.css / sq-custom.js file */
+
+function toggleFilter() {
+  let x = document.querySelector(".sq-filter-main");
+  x.classList.toggle("opening");
+}
+function toggleClass() {
+  let y = document.querySelector(".sq-filter-main");
+  y.classList.remove("opening");
+}
+function removeClass() {
+  let z = document.querySelector(".sq-filter-main");
+  z.classList.remove("opening");
+}
+
+window.sq = window.sq || {};
+
+window.sq.initSearch = (store) => {
+  store.search();
+};
+
